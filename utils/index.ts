8da643727ed4ba0f2b8a633de09df083d5a8eb3a
Mehdi Amrane <me.amrane@gmail.com>
2022-11-01 17:33:00 +0100

    Fx formatPostDate util

--- utils/index.ts
+++ utils/index.ts
@@ -1,7 +1,16 @@
+export const isJsonString = (str: string): boolean => {
+  try {
+    JSON.parse(str);
+  } catch (e) {
+    return false;
+  }
+  return true;
+};
+
 export const formatPostDate = (dateString: string, simpleLocale: 'fr' | 'en'): string => {
   const locale = simpleLocale === 'fr' ? 'fr-FR' : 'en-US';
 
-  const date = new Date(JSON.parse(dateString));
+  const date = new Date(isJsonString(dateString) ? JSON.parse(dateString) : dateString);
   const formatedDate = date.toLocaleString(locale, {
     year: 'numeric',
     month: 'short',
