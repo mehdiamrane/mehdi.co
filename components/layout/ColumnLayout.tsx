0b8aed2e9cf6287291f16732ded33789c3e6d975
Mehdi Amrane <me.amrane@gmail.com>
2022-12-12 21:04:15 +0100

    Switch from useBreakpointValue to pure css

--- components/layout/ColumnLayout.tsx
+++ components/layout/ColumnLayout.tsx
@@ -1,6 +1,6 @@
 import React, { FC, ReactNode } from 'react';
 
-import { Box, Flex, useBreakpointValue as breakpoint } from '@chakra-ui/react';
+import { Box, Flex } from '@chakra-ui/react';
 
 interface IColumnLayoutProps {
   main: ReactNode;
@@ -15,16 +15,7 @@ const ColumnLayout: FC<IColumnLayoutProps> = ({ main, aside, keepOnMobile }) => 
         flex='0 0 0'
         pt={16}
         h='100vh'
-        minW={
-          keepOnMobile === 'aside'
-            ? breakpoint({ base: '100%', md: 'xs' }, { ssr: false })
-            : breakpoint({ base: 'xs' }, { ssr: false })
-        }
-        display={
-          keepOnMobile === 'aside'
-            ? breakpoint({ base: 'block' }, { ssr: false })
-            : breakpoint({ base: 'none', lg: 'block' }, { ssr: false })
-        }
+        className={`column-layout__aside${keepOnMobile === 'aside' ? '--keep-mobile' : ''}`}
       >
         {aside}
       </Box>
@@ -32,11 +23,7 @@ const ColumnLayout: FC<IColumnLayoutProps> = ({ main, aside, keepOnMobile }) => 
         flex='1 1 0'
         overflowX='hidden'
         overflowY='scroll'
-        display={
-          keepOnMobile === 'main'
-            ? breakpoint({ base: 'block' }, { ssr: false })
-            : breakpoint({ base: 'none', sm: 'block' }, { ssr: false })
-        }
+        className={`column-layout__main${keepOnMobile === 'main' ? '--keep-mobile' : ''}`}
       >
         {main}
       </Box>
