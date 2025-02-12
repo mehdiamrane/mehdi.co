0fb885a7d0e6440148385451d5d0d72b1fc17d9b
Mehdi Amrane <me.amrane@gmail.com>
2025-02-12 21:35:14 +0100

    feat: add Umami analytics configuration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,12 +7,24 @@ import Footer from 'components/layout/Footer';
 import 'styles/globals.css';
 import { ChakraProvider } from '@chakra-ui/react';
 import theme from 'styles/theme';
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps, router }: AppProps) {
   const isAppMode = router.route.startsWith('/notes');
+  const enableAnalytics =
+    process.env.NODE_ENV === 'production' &&
+    process.env.NEXT_PUBLIC_UMAMI_WEBSITE_ID &&
+    process.env.NEXT_PUBLIC_UMAMI_SCRIPT_URL;
 
   return (
     <ChakraProvider theme={theme} resetCSS>
+      {enableAnalytics && (
+        <Script
+          defer
+          src={process.env.NEXT_PUBLIC_UMAMI_SCRIPT_URL}
+          data-website-id={process.env.NEXT_PUBLIC_UMAMI_WEBSITE_ID}
+        />
+      )}
       <NavBar router={router} isAppMode={isAppMode} />
       <Component {...pageProps} />
       <Footer isHidden={isAppMode} />
